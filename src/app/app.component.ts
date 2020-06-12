e6b35e38bc76e6325109d2fb4a7197edf0f02f9c
actionanand <meetanandns@gmail.com>
2020-06-12 20:33:55 +0530

    custom elelment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-element-example';
+
+  showMessage() {
+    const greet = document.getElementById('greet');
+    greet.innerHTML = '<app-greet></app-greet>';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements'
 
 import { AppComponent } from './app.component';
+import { GreetComponent } from './greet/greet.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GreetComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [GreetComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(private injector: Injector) {
+    const custom = createCustomElement(GreetComponent, {injector: this.injector});
+    customElements.define('app-greet',custom);
+  }
+}
